b798f34e756ee25a96a09d7f77299724a1e60195
franpautt27 <58583172+franpautt27@users.noreply.github.com>
2021-07-04 17:11:18 -0500

    added 2 more buttons

--- index.php
+++ index.php
@@ -41,6 +41,8 @@
     
     ?>
     <button type="button" class="btn btn-dark">CLICK ME!</button>
+    <button type="button" class="btn btn-primary">CLICK ME!</button>
+    <button type="button" class="btn btn-success">CLICK ME!</button>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
